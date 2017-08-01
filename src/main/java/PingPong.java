0ed158ea0b592fe8d5b199761fea3803860ee132
Beth Hansen and Bart Webber <beth@>
2017-08-01 09:21:47 -0700

    add test 3 to check for ping replace function

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,15 +1,19 @@
 
-
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class PingPong {
 
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
-        result.add(1);
+        for (int i = 1; i <= countUpTo; i++) {
+            if (i % 3 == 0) {
+                result.add("ping");
+            } else {
+                result.add(i);
+            }
+        }
         return result;
     }
-
 
 }
--- src/test/java/PingPongTest.java
+++ src/test/java/PingPongTest.java
@@ -21,4 +21,14 @@ public class PingPongTest {
         expectedOutput.add(2);
         assertEquals(expectedOutput, testPingPong.runPingPong(2));
     }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf3_ArrayList() {
+        PingPong testPingPong = new PingPong();
+        ArrayList<Object>expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        assertEquals(expectedOutput, testPingPong.runPingPong(3));
+    }
 }
