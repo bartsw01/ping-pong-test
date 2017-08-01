2e67d6cef2ebc743844f0fce171e6925219ff9f3
Beth Hansen and Bart Webber <bart@>
2017-08-01 09:55:52 -0700

    add front end user interface to enter number

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -7,11 +7,13 @@ public class PingPong {
     public ArrayList<Object> runPingPong(int countUpTo){
         ArrayList<Object> result = new ArrayList<Object>();
         for (int i = 1; i <= countUpTo; i++) {
-            if (i % 3 == 0) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("pingpong");
+            } else if (i % 3 == 0) {
                 result.add("ping");
-            } else if (i % 5 == 0) {
+            } else if (i % 5 == 0 ){
                 result.add("pong");
-            } else{
+            } else {
                 result.add(i);
             }
         }
